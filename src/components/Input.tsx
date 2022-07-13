b7c5bf12f7016930e158d5c91dbb6dd468a79936
Tuna <tunasayinn@gmail.com>
2022-07-13 21:03:03 +0300

    added enter button support for adding to-dos

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,27 +1,35 @@
 import { v4 as uuid } from 'uuid';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { InputOptions } from '../constants';
 import styles from '../styles/Input.module.scss';
 
 const Input = ({ onTodoAdd } : InputOptions) => {
   const [val, setVal] = useState<string>("");
   const onAddButtonClicked = () : void => {
-    if (val && onTodoAdd && typeof onTodoAdd === 'function') {
+    if (val.trim() && onTodoAdd && typeof onTodoAdd === 'function') {
       onTodoAdd({
         id: uuid(),
-        content: val,
+        content: val.trim(),
         createdAt: new Date(),
         isEdited: false
      });
      
       setVal("");
     }
-  }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keypress', (key) => {
+      if (key.code === 'Enter') {
+        setTimeout(onAddButtonClicked,);
+      };
+    });
+  });
 
   return (
     <div className={styles.container}>
         <div onClick={onAddButtonClicked} className={styles.btn}>+</div>
-        <input value={val} placeholder="Write something to do" onChange={(e) => setVal(e.currentTarget.value?.trim())} className={styles.input} type="text" />
+        <input value={val} placeholder="Write something to do" onChange={(e) => setVal(e.currentTarget.value)} className={styles.input} type="text" />
     </div>
   )
 }
